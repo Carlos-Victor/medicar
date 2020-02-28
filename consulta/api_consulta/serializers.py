d832fe07f0df395ad19e5eb7bf16db56788e6231
Carlos Victor <carlosvictortecprof@gmail.com>
2020-02-28 14:47:57 -0300

    Realizado a serializacao da tabela consulta

--- consulta/api_consulta/serializers.py
+++ consulta/api_consulta/serializers.py
@@ -1,37 +1,27 @@
 from rest_framework import serializers
 from consulta.models import Consultas
-from equipe.models import Medicos, Agenda
+from equipe.models import Medicos, Agenda, Especialidades
 from rest_framework.serializers import ModelSerializer, SlugRelatedField, StringRelatedField, HyperlinkedIdentityField, ValidationError
-from expander import ExpanderSerializerMixin
 
 
 
 
-
-# agenda = StringRelatedField(
-#     slug_field='dia',
-#     read_only=True,
-#     queryset=Agenda.objects.all()
-# )
-
-
-class MedicoSerializer(ExpanderSerializerMixin, serializers.ModelSerializer):
+class EspecialidadesSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Medicos
+        model = Especialidades
         fields = '__all__'
 
+class MedicoSerializer(serializers.ModelSerializer):
+    especialidade = EspecialidadesSerializer(many=False)
+    class Meta:
+        model = Medicos
+        fields = ["nome","crm","telefone","especialidade"]
 
-class ConsultasSerializer(ExpanderSerializerMixin, serializers.ModelSerializer):
+
+class ConsultasSerializer(serializers.ModelSerializer):
     dia = serializers.CharField(read_only=True, source="agenda.dia")
     horario = serializers.CharField(read_only=True, source="agenda.horarios")
-    # agenda = SlugRelatedField(
-    #     slug_field='dia',
-    #     queryset=Agenda.objects.all()
-    # )
+    medico = MedicoSerializer(source="agenda.medico", many=False)
     class Meta:
         model = Consultas
-        fields = ['paciente', 'dia','horario','data_agendamento']
-        expandable_fields = {
-            'medico': MedicoSerializer
-        } 
-        # fields = '__all__'
+        fields = ['dia','horario','data_agendamento','medico']
